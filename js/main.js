8a0bb73e8dff28a8c8de34df841a939756518517
pierfrancescoscar <scarinci401@gmail.com>
2021-11-02 15:20:57 +0100

    add grid container in js

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@
 // con difficoltà 2 => tra 1 e 81
 // con difficoltà 3 => tra 1 e 49
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+
+// Refs
+const playBtn = document.querySelector('.play');
+const chooseLevel = document.getElementById('difficoltà');
+const wrapGrid = document.querySelector('.wrap-grid');
+
+
+// Set grid
+
+const grid = document.createElement('div');
+grid.classList.add('grid');
+
+// Insert grid
+wrapGrid.append(grid);
